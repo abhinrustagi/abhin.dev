7a308eecb3b6999f891c8882c6a1029fe8a80992
abhin.rustagi <rustagi.abhin@gmail.com>
2022-06-24 12:24:45 +0530

    add under-construction page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,29 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 import Link from 'next/link'
 
 const Home: NextPage = () => {
 	return (
-		<div className="mx-auto max-w-2xl px-6">
-			<h1 className="text-5xl">Abhin Rustagi</h1>
-			<h2 className="font-medium text-lg text-neutral-300 my-3">
-				Product Engineer at{' '}
-				<Link href="https://www.openhouse.study" passHref>
-					<a
-						target="_blank"
-						rel="noopener noreferrer"
-						className="hover:text-neutral-500 text-neutral-200"
-					>
-						Openhouse
-					</a>
-				</Link>
-			</h2>
-			<p>
-				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum provident
-				quasi velit ex non dolore a ab, voluptatum perspiciatis iusto ipsum,
-				sequi repudiandae impedit ea nesciunt id aspernatur reprehenderit
-				perferendis!
-			</p>
-		</div>
+		<>
+			<Head>
+				<title>Under Construction – Abhin Rustagi</title>
+			</Head>
+			<div className="mx-auto max-w-2xl px-6">
+				<span className="text-lg mt-5 block text-amber-400">Abhin Rustagi</span>
+				<h1 className="text-5xl my-6">Under Construction</h1>
+				<p>
+					Hello, I&apos;m currently redesigning my portfolio. Please check in
+					again in some time.
+				</p>
+				<p className="mt-4">
+					Meanwhile, please feel free to contact me at{' '}
+					<Link href="mailto:hi@abhin.dev">
+						<a className="text-stone-50 font-medium underline">hi@abhin.dev</a>
+					</Link>
+					.
+				</p>
+			</div>
+		</>
 	)
 }
 
